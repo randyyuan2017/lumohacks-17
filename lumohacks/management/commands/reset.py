bc0f2f6a07eb7d3dee5a3a93c6b09b0b33084725
Randy <ryyuan@sfu.ca>
2017-09-17 12:02:30 -0700

    Added management command

--- lumohacks/management/commands/reset.py
+++ lumohacks/management/commands/reset.py
@@ -1,41 +1,24 @@
-__author__ = 'parminder'
-
 from django.core.management.base import BaseCommand, CommandError
-from optparse import make_option
-from django.db.models import Q
-from django.contrib.auth.models import User, Group
+from lumohacks.models import *
 
-# This management command will show total number of patients for each doctor
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option('--reset',
-                    action='store_true',
-                    dest='stats',
-                    default=False,
-                    help='Doctors stats'),
-
-    )
+    help = 'Closes the specified poll for voting'
 
     def handle(self, *args, **options):
-        if options['stats']:
-            self.stats()
-            self.stdout.write('*Successfully retrieved doctor stats')
-        else:
-            print "usage: python manage.py setup --help "
+        for user_activities in UserActivities.objects.all():
+            user_activities.delete()
 
-    def stats(self):
-        list_one = ['ceridwen.lloyd@sundoctors.com.au',
-                    'drsiggs@gmail.com',
-                    'lvanmac@hotmail.com',
-                    'dr.jonny.levy@gmail.com',]
+        for pet in Pet.objects.all():
+            pet.exp = 0
+            pet.save()
 
-        for list in list_one:
-            try:
-                user = User.objects.get(email=list)
-                patients = get_objects_for_user(user, 'view_patient', klass=Patient)
-                print list + " has: " + str(patients.count()) + " patients"
-                images = Image.objects.filter(sent_by_user__email=list_one)
-                print "Images Upload by Doctor: " + str(len(images))
+        for user in PetUser.objects.all():
+            user.money = 0
+            user.save()
 
-            except:
-                print "Doctor not found into system: " + str(list)
+        for gift in Gift.objects.all():
+            gift.purchased = False
+            gift.save()
+
+
+        self.stdout.write(self.style.SUCCESS('Successfully reset db'))
