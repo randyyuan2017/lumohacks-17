586e3d35f80d325de84860b474907c3a4b28c77c
Randy <ryyuan@sfu.ca>
2017-09-17 10:08:20 -0700

    added allowed host

--- lumohacks/settings.py
+++ lumohacks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9s-7nksoj=c)kdpz2vj8kcy0-oxmc-azur0a59vr1k$if0@!p)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
