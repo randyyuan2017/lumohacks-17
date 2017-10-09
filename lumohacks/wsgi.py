3ed77f436f100cffba36026a006aa01595d5321e
Ubuntu <ubuntu@ip-172-31-30-68.us-west-2.compute.internal>
2017-10-09 03:11:52 +0000

    added requirement files and modified wsgi file

--- lumohacks/wsgi.py
+++ lumohacks/wsgi.py
@@ -7,7 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+# add the hellodjango project path into the sys.path
+sys.path.append('/home/ubuntu/lumohacks-17/lumohacks')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/ubuntu/lumohacks-17/ENV/Lib/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
